8191da98813ba3996fa0a1140e65b237448d0abf
Sören Hentzschel <kontakt@agenedia.com>
2019-07-08 20:00:17 +0200

    get the full bookmark paths

--- src/js/core/background.js
+++ src/js/core/background.js
@@ -14,6 +14,13 @@ const kodb = {
   collectedBookmarks : [],
 
   /**
+   * Additional data stored for bookmarks. In current version it only contains the full bookmark path.
+   *
+   * @type {Array.<string>}
+   */
+  additionalData : [],
+
+  /**
    * Fired when the toolbar icon is clicked. This method is used to open the user interface in a new tab or to switch
    * to the tab with the user interface if the user interface is already opened.
    *
@@ -55,6 +62,37 @@ const kodb = {
   },
 
   /**
+   * Calculates the full path of bookmarks.
+   *
+   * @param {bookmarks.BookmarkTreeNode} bookmark - a single bookmark
+   * @param {Array.<string>} path - an array with parts of the path of the bookmark
+   *
+   * @returns {Array.<string>} - An array with the full path of all bookmarks
+   */
+  calculateBookmarkPaths (bookmark, path) {
+    if (bookmark.title) {
+      path.push(bookmark.title);
+    }
+
+    if (bookmark.children) {
+      for (const child of bookmark.children) {
+        kodb.calculateBookmarkPaths(child, path);
+      }
+    }
+    else {
+      if (!kodb.additionalData[bookmark.id]) {
+        kodb.additionalData[bookmark.id] = {};
+      }
+
+      kodb.additionalData[bookmark.id].path = path.slice(0, -1);
+    }
+
+    path.pop();
+
+    return kodb.additionalData;
+  },
+
+  /**
    * This method is used to start collecting all bookmarks.
    *
    * @returns {void}
@@ -63,6 +101,7 @@ const kodb = {
     const bookmarks = await browser.bookmarks.getTree();
 
     kodb.collectedBookmarks = [];
+    kodb.calculateBookmarkPaths(bookmarks[0], []);
     kodb.collectBookmark(bookmarks[0]);
   },
 
@@ -77,7 +116,9 @@ const kodb = {
     // we only collect bookmarks, no folders or seperators
     if (bookmark.type === 'bookmark') {
       const { id, title, url } = bookmark;
-      kodb.collectedBookmarks.push({ id, title, url });
+      const { path } = kodb.additionalData[id];
+
+      kodb.collectedBookmarks.push({ id, title, url, path });
     }
 
     if (bookmark.children) {
